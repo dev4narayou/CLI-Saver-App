42bfa4d7218022cd5cb83e48331c5293dc07ff30
dev <devmanojkumar08@gmail.com>
2025-02-26 18:06:36 +1100

    Updated Dependencies, adjusted port for development.

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = require("./app");
 
-const port = 3000;
+const port = 3005;
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
