b09abce3e9719d045da2c807f9081cf780d6b45a
dev <devmanojkumar08@gmail.com>
2025-03-11 21:17:38 +1100

    updated PORT

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const app = require("./app");
 
-const port = 3005;
+const PORT = process.env.PORT || 3005;
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
